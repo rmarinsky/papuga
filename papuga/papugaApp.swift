1a4ccb4cd99b1215e65e3ac337ece527e27ec2ab
Roman Marinsky <newromka@gmail.com>
2026-02-11 20:28:08 +0200

    Initial Commit

---
+++ papuga/papugaApp.swift
@@ -0,0 +1,17 @@
+//
+//  papugaApp.swift
+//  papuga
+//
+//  Created by Roman Marinskyi on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct papugaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
